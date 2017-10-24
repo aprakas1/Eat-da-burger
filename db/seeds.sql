0eed084ae8930c64de8dbf6d32fdf32960a80855
Arushi Prakash <arushiprakash@hotmail.com>
2017-10-24 16:35:01 -0400

    handlebars works but I cannot post the data

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_tbl(burger_name) values ('Say Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Tunami');
+insert into burgers_tbl(burger_name) values ('Rest in Peas Burger');
+insert into burgers_tbl(burger_name) values ('Ive Created a Muenster Burger');
